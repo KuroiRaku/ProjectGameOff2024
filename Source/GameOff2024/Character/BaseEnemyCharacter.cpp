ecb9286ee5fd084f9d456692a0cbad9add5f64ec
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2024-11-02 20:31:48 -0400

    Setup Attachment To GetCapsule Component

--- Source/GameOff2024/Character/BaseEnemyCharacter.cpp
+++ Source/GameOff2024/Character/BaseEnemyCharacter.cpp
@@ -12,7 +12,7 @@ ABaseEnemyCharacter::ABaseEnemyCharacter()
 	Controller = CreateDefaultSubobject<AAIController>("AIController");
 
 	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
-	SkeletalMesh->SetupAttachment(FirstPersonCameraComponent);
+	SkeletalMesh->SetupAttachment(GetCapsuleComponent());
 	SkeletalMesh->bCastDynamicShadow = false;
 	SkeletalMesh->CastShadow = false;
 	//Mesh1P->SetRelativeRotation(FRotator(0.9f, -19.19f, 5.2f));
